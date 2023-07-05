d6eb6c04bd25f998af270fddb0327e4fac1428c8
Sahil Sayyad <96423459+Sahil-Sayyad@users.noreply.github.com>
2023-07-05 11:31:23 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 //import all required packages 
 const mongoose = require('mongoose');
 
-const mongoDBurl = "mongodb+srv://sahilsayyad:9325@todoapp.bx2zyst.mongodb.net/?retryWrites=true&w=majority"
+const mongoDBurl = "mongodb+srv://<yourusrname>:<yourpassword>@todoapp.bx2zyst.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(mongoDBurl)
 .then(()=> console.log("MongoDb conneted successfuly"))
